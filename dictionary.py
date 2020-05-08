09407c143a6a59f38b69600a885423191a29ec01
rakshitha18 <65013688+rakshitha18@users.noreply.github.com>
2020-05-08 16:54:57 +0530

    removing elements from dictionaries

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+test_dict = {"diya" : 22, "anu" : 21, "Mani" : 21, "taqee" : 21}
+print ("The dictionary before performing remove is : " + str(test_dict)) 
+del test_dict['Mani'] 
+print ("The dictionary after remove is : " + str(test_dict)) 
